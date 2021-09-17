e67c8bbff58f15210f8a687ebdf5d015fef32b55
Ferdinand Malcher <ferdinand@malcher.media>
2021-09-17 10:54:06 +0200

    Test Dashboard mit Service-Stub

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { BookComponent } from './books/book/book.component';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let book: Book;
+  let ratingMock: BookRatingService;
 
   beforeEach(async () => {
+    book = {
+      isbn: '',
+      title: '',
+      rating: 3,
+      price: 3,
+      description: '',
+      authors: []
+    };
+
+    ratingMock = {
+      rateUp: book => book,
+      rateDown: book => book
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        // immer wenn BRS angefordert wird, wird stattdessen ratingMock ausgeliefert
+        { provide: BookRatingService, useValue: ratingMock }
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +43,19 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the service for doRateUp()', () => {
+    // Arrange
+    const rs = TestBed.inject(BookRatingService);
+    // callThrough: Aufruf soll an originales Objekt "rs" durchgeleitet werden
+    spyOn(rs, 'rateUp').and.callThrough();
+
+    // Act
+    component.doRateUp(book);
+
+    // Assert
+    expect(rs.rateUp).toHaveBeenCalledTimes(1);
+    expect(rs.rateUp).toHaveBeenCalledWith(book);
   });
 });
