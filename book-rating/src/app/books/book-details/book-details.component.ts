389369fed5851b1bfb067c65e5b6bace7ec99c9f
Ferdinand Malcher <ferdinand@malcher.media>
2021-09-22 12:07:49 +0200

    Detailseite: Higher-Order Observables

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'br-book-details',
@@ -18,11 +19,13 @@ export class BookDetailsComponent implements OnInit {
 
     // Asynchroner Weg
     // TODO: Verschachtelte Subscription
-    this.route.paramMap.subscribe(params => {
-      const isbn = params.get('isbn');
-      this.bs.getSingle(isbn!).subscribe(book => {
-        this.book = book;
-      });
+
+
+    this.route.paramMap.pipe(
+      map(params => params.get('isbn')),
+      switchMap(isbn => this.bs.getSingle(isbn!))
+    ).subscribe(book => {
+      this.book = book;
     });
 
   }
